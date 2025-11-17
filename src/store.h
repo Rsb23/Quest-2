980cb420f30352ec62aa9d97df6ec772d964e570
bog <francisstephan912@gmail.com>
2025-11-17 11:54:39 -0500

    Create store.h

---
+++ src/store.h
@@ -0,0 +1,28 @@
+#ifndef STORE_H
+#define STORE_H
+
+#include <iostream>
+
+class Store{
+private:
+    //variables 
+    int itemChoice;
+    int itemCount;
+    int points{50}; // I set at 50 for testing, will change back for integration
+    int cueCardCount{0};
+    int supremeCardCount{0};
+    
+public:
+    // declare the functi9ns 
+    bool StoreMenu();
+    void PurchaseCC();
+    void PurchaseSCC();
+    void showInventory();
+
+    // getters and setters
+    int getPoints() const { return points; }
+    void setPoints(int points); 
+    
+};
+
+#endif
